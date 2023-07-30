027b593d69b02b595af58966f7abec4025d00e75
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2023-07-29 21:41:06 -0700

    tests to come

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,20 @@ int main(){
 
     double beta = 20;
     double h = 0;
-    double t = 1.0;
     int    nt = 200;
     auto times = nda::zeros<double>(nt);
     for (int i=0; i < nt; i++){ times(i) = beta * (double)i/(nt-1); }
 
-    Configuration c = Configuration(nda::vector<double>{1.0, 3.0},
-                                    nda::vector<double>{2.0, 4.0}
-            );
-
     GreensFunction g_ref = read_semi_circular_g_tau();
-    Hybridization Delta = Hybridization(times, -0.25*t*t*g_ref.data, beta);
+    Hybridization Delta = Hybridization(times, -0.25*g_ref.data, beta);
+    auto e = Expansion(beta, h, Delta);
+    auto c = Configuration(nda::vector<double>{1.0}, nda::vector<double>{3.0} );
+    auto d = Determinant(c, e);
+    auto t = trace(c, e);
+
+    std::cout << "d.mat = " << d.mat << std::endl;
+    std::cout << "d.val = " << d.value << std::endl;
+    std::cout << "trace = " << t << std::endl;
 
   return 0;
 }
